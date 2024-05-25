8845317120ec048a684269fda39543c882f29d21
melb.jeds <melb.jeds@gmail.com>
2024-05-25 21:19:31 +0700

    style: 💄 lint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: 'tsconfig.json',
+		tsconfigRootDir: __dirname,
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint/eslint-plugin', 'import', 'unused-imports'],
+	extends: ['plugin:@typescript-eslint/recommended'],
+	root: true,
+	env: {
+		node: true,
+		jest: true,
+	},
+	ignorePatterns: ['.eslintrc.js', 'dist', 'node_modules'],
+	rules: {
+		'@typescript-eslint/interface-name-prefix': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'import/order': [
+			'error',
+			{
+				'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+				'alphabetize': {
+					order: 'asc',
+					caseInsensitive: true,
+				},
+				'newlines-between': 'always',
+			},
+		],
+		'import/no-unused-modules': 'error',
+		'import/no-unassigned-import': 'error',
+		'no-unused-vars': 'off',
+		'unused-imports/no-unused-imports': 'error',
+		'unused-imports/no-unused-vars': [
+			'warn',
+			{
+				vars: 'all',
+				varsIgnorePattern: '^_',
+				args: 'after-used',
+				argsIgnorePattern: '^_',
+			},
+		],
+	},
+}
